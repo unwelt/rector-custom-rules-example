40ac2f8415f253d901fcd94509bd3f4db7051018
sergey.saharov <sergey.saharov@lamoda.ru>
2025-03-06 12:22:21 +0700

    Added enum for example

---
+++ src/PaillechatEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RectorExample;
+
+use Paillechat\Enum\Enum;
+
+final class PaillechatEnum extends Enum
+{
+    protected const EXAMPLE_CONST_1 = 1;
+    protected const EXAMPLE_CONST_2 = 2;
+}
