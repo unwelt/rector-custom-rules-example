497b8c23caa67d1a8f12839c19b258527202d30c
sergey.saharov <sergey.saharov@lamoda.ru>
2025-03-15 16:23:20 +0700

    Added PHPUnit and first test

---
+++ utils/rector/tests/Rector/PaillechatEnumMethodCallToEnumConstRector/PaillechatEnumMethodCallToEnumConstRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Utils\Rector\Tests\Rector\PaillechatEnumMethodCallToEnumConstRector;
+
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+class PaillechatEnumMethodCallToEnumConstRectorTest extends AbstractRectorTestCase
+{
+
+    public function provideConfigFilePath(): string
+    {
+        // TODO: Implement provideConfigFilePath() method.
+    }
+}
